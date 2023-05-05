cab913d5c5ab459b7cd3dc3fe11d8d81ea9d24f9
Noah Harper <114954963+noahlikesvr@users.noreply.github.com>
2023-05-05 18:37:02 -0400

    Add files via upload

---
+++ JS/Spin.js
@@ -0,0 +1,21 @@
+// var spin = new Audio('Music/Spin Eternally/SpinSpinSpinSpin.mp3');
+// var MaETa = new Audio('Music/Spin Eternally/Ma e ta.mp3');
+// var Full = new Audio('Music/Spin Eternally/SpinSpinSpinSpinSpin.mp3');
+
+function Spin()
+{
+	var image = document.getElementById("spin");
+	image.classList.add("spin");
+	spin.volume = 0.5;
+	spin.play();
+	setTimeout(() => 
+	{
+    image.classList.remove("spin");
+	}, 1000);
+	
+} 
+
+function rng() {
+  return Math.floor(Math.random() * 3);
+}
+
